f585671b175b36df2905151a0163bc1437190098
“LoveEleve” <2055384004@qq.com>
2025-12-29 15:57:51 +0800

    feat: 添加本地编译配置和调试模块

---
+++ spring-debug/src/main/java/com/debug/DebugApplication.java
@@ -0,0 +1,34 @@
+package com.debug;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.debug")
+public class DebugApplication {
+
+	public static void main(String[] args) {
+		// 断点1: 容器创建入口 - 可以跟踪 refresh() 方法
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DebugApplication.class);
+
+		// 断点2: 获取 Bean - 可以跟踪 getBean() 流程
+		HelloService helloService = context.getBean(HelloService.class);
+		helloService.hello();
+
+		context.close();
+	}
+
+	@Bean
+	public HelloService helloService() {
+		return new HelloService();
+	}
+
+	public static class HelloService {
+		public void hello() {
+			System.out.println("Debug Spring Source!");
+		}
+	}
+}
